79a65c90589af704b8de86f1361361bb1aa99bee
Seungho Choi <anteaterho@gmail.com>
2015-12-24 09:16:18 +0900

    Object rotation by mouse drag
    마우스 드래그에따른 오브젝트 회전

---
+++ Assets/Scenes/Rotation/Script/NewBehaviourScript.js
@@ -0,0 +1,17 @@
+﻿var speed : int;
+var friction : float;
+var lerpSpeed : float;
+private var xDeg : float;
+private var yDeg : float;
+private var fromRotation : Quaternion;
+private var toRotation : Quaternion;
+ 
+function Update () {
+    if(Input.GetMouseButton(0)) {
+        xDeg -= Input.GetAxis("Mouse X") * speed * friction;
+        yDeg += Input.GetAxis("Mouse Y") * speed * friction;
+        fromRotation = transform.rotation;
+        toRotation = Quaternion.Euler(yDeg,xDeg,0);
+        transform.rotation = Quaternion.Lerp(fromRotation,toRotation,Time.deltaTime  * lerpSpeed);
+    }
+}
---
+++ Assets/Scenes/Rotation/Script/ObjectRotationByMouseDrag.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectRotationByMouseDrag : MonoBehaviour {
+
+    public Vector2 clickPos;
+    public Vector2 offsetPos;
+    public float divider = 80;
+
+	// Use this for initialization
+	void Start () {
+        clickPos = new Vector2(0, 0);
+        offsetPos = new Vector2(0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offsetPos = new Vector2(0, 0);
+
+        if(Input.GetKeyDown(leftClick()))
+        {
+            clickPos = MouseXY();
+        }
+
+        if(Input.GetKey(leftClick()))
+        {
+            offsetPos = clickPos - MouseXY();
+        }
+
+        this.transform.Rotate(new Vector3(-(offsetPos.y / divider), offsetPos.x / divider, 0.0f), Space.World);
+	}
+
+    KeyCode leftClick()
+    {
+        return KeyCode.Mouse0;
+    }
+
+    Vector2 MouseXY()
+    {
+        Vector2 mouseXY = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        return mouseXY;
+    }
+
+    float mouseX()
+    {
+        return Input.mousePosition.x;
+    }
+
+    float mouseY()
+    {
+        return Input.mousePosition.y;
+    }
+}
