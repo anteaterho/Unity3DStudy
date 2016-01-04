5e0d0c726902e70f0297f48e7d52178ea36d5d03
Seungho Choi <anteaterho@gmail.com>
2016-01-04 18:51:30 +0900

    오브젝트 로테이션과 UDP매니저 추가.

---
+++ Assets/Scenes/Rotation/Script/ObjectRotation.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ObjectRotation : MonoBehaviour
+{
+
+
+    public float rotationSpeed = 10.0f;
+    public float lerpSpeed = 30.0f;
+    public Text temptext;
+    public Text OutText;
+    private Vector3 speed = new Vector3();
+    private Vector3 avgSpeed = new Vector3();
+    private bool dragging = false;
+    private Quaternion rot = Quaternion.identity;
+
+    void Start()
+    {
+        Input.ResetInputAxes();
+
+
+        OutText.text = "Wow!!";
+    }
+    void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+
+    public Vector2 nowPos, prePos;
+    public Vector3 movePos;
+    void Update()
+    {
+
+
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                prePos = touch.position - touch.deltaPosition;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                speed = new Vector3(-Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+                temptext.text = speed.x + ":" + speed.y + ":" + speed.z;
+                avgSpeed = Vector3.Lerp(avgSpeed, speed, Time.deltaTime * 2);
+                transform.Rotate(Camera.main.transform.up * speed.x * rotationSpeed, Space.World);
+                transform.Rotate(Camera.main.transform.right * speed.y * rotationSpeed, Space.World);
+                rot = transform.rotation;
+                OutText.text = "T_T";
+                GameObject.Find("UDPControler").GetComponent<UDPManager>().sendString(rot.x + ":" + rot.y + ":" + rot.z + ":" + rot.w);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                if (dragging)
+                {
+                    speed = avgSpeed;
+                    dragging = false;
+                }
+
+                float i = Time.deltaTime * lerpSpeed;
+                speed = Vector3.Lerp(speed, Vector3.zero, i);
+
+                //			if(speed == Vector3.zero)
+                {
+                    Input.ResetInputAxes();
+
+                    OutText.text = "Wow!!";
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scenes/UDP/Script/UDPManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+public class UDPManager : MonoBehaviour
+{
+
+    public string IP;  // define in init
+    public int port;  // define in init
+
+    // "connection" things
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    // Use this for initialization
+    void Start()
+    {
+        IP = "192.168.203.45";
+        port = 13579;
+
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+        print("Sending to " + IP + " : " + port);
+        print("Testing: nc -lu " + IP + " : " + port);
+    }
+
+
+    public void sendString(string message)
+    {
+        try
+        {
+
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(IP), port);
+            //MessageCoder.encode(msg) is returning a valid byte[], no problem here.
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            client.Send(data, data.Length, iep);
+            //if (message != "")
+            //{
+
+            //			// Encode data using the UTF8 encoding to binary format.
+            //			byte[] data = Encoding.UTF8.GetBytes(message);
+            //			
+            //			// Send the message to the remote client.
+            //			client.Send(data, data.Length, remoteEndPoint);
+            //}
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
