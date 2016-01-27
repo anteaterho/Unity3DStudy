602bc793f9e6c47c97b1e14e68e2636e58e99dd9
Seungho Choi <anteaterho@gmail.com>
2016-01-27 23:27:02 +0900

    projectionMatrix 테스트 시작

---
+++ Assets/Scenes/ProjectionMatrix/Script/CameraProjection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraProjection : MonoBehaviour {
+
+	public Matrix4x4 originalProjection;
+	Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Matrix4x4 p = originalProjection;
+		p.m01 += Mathf.Sin (Time.time * 1.2F) * 0.1f;
+		p.m10 += Mathf.Sin (Time.time * 1.5f) * 0.1f;
+		camera.projectionMatrix = p;
+	}
+}
